f532c9fe3338fd1c40dab42ebe0b9006cb6ffa87
qweewq <seongkh15@naver.com>
2024-01-28 17:53:33 +0900

    client/server

---
+++ server/src/main/java/com/react/crud/reactcrud/ReactcrudApplication.java
@@ -0,0 +1,13 @@
+package com.react.crud.reactcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactcrudApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/react/crud/reactcrud/ReactcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react.crud.reactcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
