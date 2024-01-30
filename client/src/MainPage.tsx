e84336ad73f33032863db48f8750ed11b6061751
qweewq <seongkh15@naver.com>
2024-01-30 20:13:23 +0900

    css 수정, 엔터키 추가

--- client/src/MainPage.tsx
+++ client/src/MainPage.tsx
@@ -1,20 +1,20 @@
-import Active from './component/Active';
-import Done from './component/Done';
-import Input from './component/Input';
-import Title from './component/Title';
-import './index'
+import Active from "./component/Active";
+import Done from "./component/Done";
+import Input from "./component/Input";
+import Title from "./component/Title";
+import "./index";
 
 const MainPage = () => {
   return (
-    <div className='mainpage'>
+    <div className="mainpage">
       <Title />
       <Input />
-      <div className='todolist'>
-      <Active />
-      <Done />
+      <div className="todolist">
+        <Active />
+        <Done />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MainPage;
--- client/src/component/Input.tsx
+++ client/src/component/Input.tsx
@@ -1,37 +1,44 @@
-import React, { useState, useRef } from 'react';
-import { useStore } from '../store/store';
+import React, { useState, useRef, KeyboardEvent } from "react";
+import { useStore } from "../store/store";
 
 const Input = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const inputRef = useRef<HTMLInputElement>(null);
-  
+
   const { addTodo } = useStore();
-  
-  const handleInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-  }
+  };
 
   const handleAddTodo = () => {
     if (inputValue.trim()) {
       addTodo(inputValue.trim());
-      setInputValue('');
+      setInputValue("");
       inputRef.current?.focus();
     }
-  }
+  };
 
-  return(
-    <div className='todoInput'>
-      <input type='text'
+  const enterAdded = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="todoInput">
+      <input
+        type="text"
         onChange={handleInput}
-        placeholder='할 일을 입력해주세요'
+        onKeyDown={enterAdded}
+        placeholder="할 일을 입력해주세요"
         value={inputValue}
         ref={inputRef}
       />
       <button onClick={handleAddTodo}>추가</button>
     </div>
-  )
-
-}
+  );
+};
 
 export default Input;
