ccea10e8682d8f8690ddf1d60420099708a38340
qweewq <seongkh15@naver.com>
2024-01-30 20:30:22 +0900

    추가시 자동 스크롤

--- client/src/component/Done.tsx
+++ client/src/component/Done.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useStore } from "../store/store";
 import TodoTask from "./TodoTask";
 import "./component.css";
@@ -5,8 +6,16 @@ import "./component.css";
 const Done = () => {
   const { todos } = useStore();
 
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [todos]);
+
   return (
-    <div className="activetitle">
+    <div className="activetitle" ref={scrollRef}>
       <h1>Done</h1>
       <div className="activeline"></div>
       {todos.map((todo) =>
